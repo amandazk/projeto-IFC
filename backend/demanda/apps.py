759a79002ffee33367306604fa558327b0d489de
PietroDC12 <52581321+PietroDC12@users.noreply.github.com>
2020-11-13 10:05:18 -0300

    update_demanda_apps

--- backend/demanda/apps.py
+++ backend/demanda/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+from django.apps import apps
 
+model = apps.get_model('pessoas', 'TbPessoa')
+model = apps.get_model('servico', 'Tbservico')
 
 class DemandaConfig(AppConfig):
     name = 'demanda'
