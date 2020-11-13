eb845d43e2a84f5a7e1a900e3b3c708123c1fad2
amandazk <zacharukaz@gmail.com>
2020-11-13 10:30:32 -0300

    models e admin voluntariado

--- backend/voluntariado/admin.py
+++ backend/voluntariado/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from voluntariado.models import TbVoluntariado
 
-# Register your models here.
+class Voluntariado(admin.ModelAdmin):
+    list_display = ('voluntariado_id', 'desc_voluntariado')
+    list_display_links = ('voluntariado_id', )
+    list_per_page = 10
+
+admin.site.register(TbVoluntariado, Voluntariado)
--- backend/voluntariado/models.py
+++ backend/voluntariado/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class TbVoluntariado (models.Model):
+    objects = models.Manager() #evitar erro no vscode
+    voluntariado_id = models.AutoField(primary_key=True)
+    desc_voluntariado = models.TextField(blank=False)
+
+    pessoa = models.ForeignKey(
+        'pessoas.TbPessoa',
+        related_name='voluntariado',
+        on_delete = models.CASCADE 
+    )
+
+    # servico = models.ManyToManyField (
+    #     'servicos.TbServico',
+    #     related_name='voluntariado'
+    # )
+    
