c427d2967cba125fe73543c9b2a7c83ddaeca19f
PietroDC12 <52581321+PietroDC12@users.noreply.github.com>
2020-11-14 09:51:35 -0300

    models_noticia

---
+++ backend/noticia/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class TbNoticia(models.Model):
+    objects = models.Manager() #evitar erro no vscode
+    noticia_id =  models.AutoField(primary_key=True)
+    titulo_noticia = models.CharField(max_length=300, blank=False)
+    subtitulo_noticia = models.CharField(max_length=300, blank=False)
+    desc_noticia = models.TextField(max_length=10000, blank=False)
+
+    pessoas = models.ForeignKey(
+        'pessoas.TbPessoa',
+        related_name='noticia',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.titulo_noticia
+
+    class Meta:
+        verbose_name = 'Noticia'
+        verbose_name_plural = Noticiass'
