0b67b02d2542c47075459be1fba59e39f644c326
amandazk <zacharukaz@gmail.com>
2020-11-16 09:12:07 -0300

    Models servico

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'pessoas',
     'voluntariado',
     'demanda',
+    'noticia',
+    'oferta',
+    'servico',
 ]
 
 MIDDLEWARE = [
--- backend/servico/models.py
+++ backend/servico/models.py
@@ -6,6 +6,22 @@ class TbServico(models.Model):
     titulo_servico = models.CharField(max_length=100, blank=False)
     desc_servico = models.TextField(max_length=1400, blank=False)
 
+    oferta = models.ManyToManyField(
+        'oferta.TbOferta',
+        related_name='servicos'
+    )
+    
+    demanda = models.ManyToManyField(
+        'demanda.TbDemanda',
+        related_name='demandas'
+    )
+    
+    voluntariado = models.ManyToManyField(
+        'voluntariado.TbVoluntariado',
+        related_name='voluntariados'
+    )
+    
+    
     def _str_(self):
         return self.titulo_servico
     class Meta:
