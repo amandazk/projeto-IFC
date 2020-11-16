744410596de021c37d658cc7131c3eda3255939d
amandazk <zacharukaz@gmail.com>
2020-11-16 10:51:02 -0300

    settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'phonenumber_field',
+    'django.contrib.gis',
     'campanhas',
     'pessoas',
     'voluntariado',
