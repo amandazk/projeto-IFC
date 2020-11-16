d113f7a106bb2ed5d073697a5d43fcd9b2ed575e
amandazk <zacharukaz@gmail.com>
2020-11-16 13:37:44 -0300

    postgis settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'projeto',
         'USER': 'postgres',
         'PASSWORD': 'pgadminaz123',
