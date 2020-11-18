e8358e874fa6818ef16b51c01281b52de86dba17
amandazk <zacharukaz@gmail.com>
2020-11-18 10:12:29 -0300

    Testando navbar no frontend

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav-bar-component />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavBarComponent from './components/NavBarComponent'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    NavBarComponent
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
