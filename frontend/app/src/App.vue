8fe355c451d0c38d46bf098ae974642cdbba98f1
PietroDC12 <52581321+PietroDC12@users.noreply.github.com>
2020-11-23 16:40:42 -0300

    inicio_home

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -11,7 +11,7 @@
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">COVID-19</a>
+        <a class="nav-link" href="#">Notícias</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Campanhas</a>
