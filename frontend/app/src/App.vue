e207de85574172e9a4c6ecdf9c83544c1a00f083
PietroDC12 <52581321+PietroDC12@users.noreply.github.com>
2020-11-25 14:35:54 -0300

    nav_bar_separada

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,35 +1,10 @@
 <template>
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-success">
-
-  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-    <ul class="navbar-nav mr-auto">
-      <a class="navbar-brand" href="#">
-    <img src="./assets/pequno.png" width="160" height="40" class="d-inline-block align-top" alt="">
-      </a>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Notícias</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Campanhas</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Serviços</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Iniciativas</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Cadastro</a>
-      </li>
-      <li class="form-inline my-2 my-lg-0">
-        <a class="btn btn-outline-success my-2 my-sm-0" href="#">Entrar</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+ <v-app id="">
+    <NavBar ref="drawer"></NavBar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 
   <div> <a> aaaaaa </a> </div>
   <div> <a> aaaaaa </a> </div>
@@ -98,16 +73,20 @@
 
 <script>
 
+import NavBar from "./components/NavBar.vue"
 
 export default {
   name: 'App',
-  
-}
+  components: {
+    NavBar: NavBar
+    }
+  }
+
 </script>
 
 -<style>
 .footer-bs {
-    background-color: rgba(181, 205, 172, 100);
+  background-color: rgba(181, 205, 172, 100);
 	padding: 60px 40px;
 	color: rgba(71,82,94,100);
 	margin-bottom: 20px;
