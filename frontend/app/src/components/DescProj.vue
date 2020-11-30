eb8cd42a2058939b231b36632d22b78a39732fc6
amandazk <zacharukaz@gmail.com>
2020-11-30 15:56:05 -0300

    Erro do descrição

--- frontend/app/src/components/DescProj.vue
+++ frontend/app/src/components/DescProj.vue
@@ -1,42 +1,43 @@
 <template>
-   <v-app id="">
-    <NavBar ref="drawer"></NavBar>
-    <v-content>
-      <router-view></router-view>
-    </v-content>
-  </v-app>
- 
-    <h1> a </h1>
-    <h1> a </h1>
- 
- 
-    <h1> a </h1>
-    <h1> a </h1><h1> a </h1>
-    <h1> a </h1><h1> a </h1><h1> a </h1>
- 
- 
- 
-   <v-app id="">
-    <Footer ref="drawer"></Footer>
-    <v-content>
-      <router-view></router-view>
-    </v-content>
-  </v-app>
+  <div>
+    <v-app id="">
+      <NavBar ref="drawer"></NavBar>
+      <v-content>
+        <router-view></router-view>
+      </v-content>
+    </v-app>
+
+    <h1>a</h1>
+    <h1>a</h1>
+
+    <h1>a</h1>
+    <h1>a</h1>
+    <h1>a</h1>
+    <h1>a</h1>
+    <h1>a</h1>
+    <h1>a</h1>
+
+    <v-app id="">
+      <Footer ref="drawer"></Footer>
+      <v-content>
+        <router-view></router-view>
+      </v-content>
+    </v-app>
+  </div>
 </template>
  
 <script>
- 
-import NavBar from "./NavBar"
-import Footer from "./Footer"
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 // import axios from "axios";
 // import router from "@/router/"
- 
+
 export default {
-  name: 'DescProj',
+  name: "DescProj",
   components: {
     NavBar: NavBar,
-    Footer: Footer
-    }
-}
+    Footer: Footer,
+  },
+};
 </script>
  
