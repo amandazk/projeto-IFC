ccf386bc011c104c0e077228500ae70bedc4dd6b
amandazk <zacharukaz@gmail.com>
2020-12-02 12:36:24 -0300

    List demandas

---
+++ frontend/app/src/components/Demanda/List.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Lista de Demandas</h1>
+    <hr />
+    <ul>
+      <li v-for="demanda in demandas" :key="demanda.demanda_id">
+        {{ demanda.titulo_demanda }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "DemandaList",
+  data() {
+    return {
+      newDemanda: "",
+      demandas: [],
+    };
+  },
+  mounted() {
+    // na hora que a página for montada, quero exibir o resultado do banco
+    this.getDemanda();
+  },
+  methods: {
+    getDemanda() {
+      axios.get("http://localhost:8000/api/demanda").then((res) => {
+        // console.log(res.data);
+        this.demandas = res.data;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- frontend/app/src/router/index.js
+++ frontend/app/src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
 
+import Index from '../components/Index.vue'
 import DescProj from '../components/DescProj.vue'
 import CampanhaList from '../components/Campanha/List.vue'
-import Index from '../components/Index.vue'
+import DemandaList from '../components/Demanda/List.vue'
 
 Vue.use(VueRouter)
 
@@ -25,7 +26,13 @@ export default new VueRouter({
       name: 'CampanhaList',
       component: CampanhaList
     },
+    {
+      path: '/demandas',
+      name: 'DemandaList',
+      component: DemandaList
+    }
 
 
 
-  ]})
+  ]
+})
