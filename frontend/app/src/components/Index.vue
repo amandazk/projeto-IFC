d3984fc88015d8a45167030c21b29637ba6bf23a
amandazk <zacharukaz@gmail.com>
2020-12-05 19:01:05 -0300

    Index

--- frontend/app/src/components/Index.vue
+++ frontend/app/src/components/Index.vue
@@ -1,106 +1,123 @@
 <template>
-  <div>
-    <v-app id="">
-      <NavBar ref="drawer"></NavBar>
-      <v-content>
-        <router-view></router-view>
-      </v-content>
-    </v-app>
-
-    <div class="container">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="card bg-light mb-3 col-md-4">
-           <div class="card-body">
-            <h2 class="card-title">Light card title</h2>
-            <p class="card-text">
-            Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
-            tellus ac cursus commodo, tortor mauris condimentum nibh, ut
-            fermentum massa justo sit amet risus. Etiam porta sem malesuada
-            magna mollis euismod. Donec sed odio dui.
-            </p>
-           </div>
-          <p>
-            <a
-              class="btn btn-secondary p-2 mb-2 bg-success text-white"
-              href="./descricao"
-              role="button"
-              >Saiba mais &raquo;</a
-            >
-          </p>
-        </div>
-        <div class="card bg-light mb-3 col-md-4">
-          <div class="card-body">
-            <h2 class="card-title">Light card title</h2>
-            <p class="card-text">
-            Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
-            tellus ac cursus commodo, tortor mauris condimentum nibh, ut
-            fermentum massa justo sit amet risus. Etiam porta sem malesuada
-            magna mollis euismod. Donec sed odio dui.
-          </p>
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-lg-4 col-sm-10 mb-4 text-center">
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal" id="title">Card text</h4>
           </div>
-          <p>
-            <a
-              class="btn btn-secondary p-2 mb-2 bg-success text-white"
-              href="/informacao-projeto"
-              role="button"
-              >Saiba mais &raquo;</a
-            >
-          </p>
-        </div>
-        <div class="card bg-light mb-3 col-md-4">
           <div class="card-body">
-            <h2 class="card-title">Light card title</h2>
-            <p class="card-text">
-            Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
-            tellus ac cursus commodo, tortor mauris condimentum nibh, ut
-            fermentum massa justo sit amet risus. Etiam porta sem malesuada
-            magna mollis euismod. Donec sed odio dui.
-          </p>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>
+                <p id="text-card">
+                  Donec id elit non mi porta gravida at eget metus. Fusce
+                  dapibus, tellus ac cursus commodo, tortor mauris condimentum
+                  nibh, ut fermentum massa justo sit amet risus. Etiam porta sem
+                  malesuada magna mollis euismod. Donec sed odio dui
+                </p>
+                <p>
+                  <a
+                    class="btn btn-secondary p-2 mb-2 bg-success text-white"
+                    href="/cuidados-covid"
+                    role="button"
+                    >Saiba mais &raquo;</a
+                  >
+                </p>
+              </li>
+            </ul>
           </div>
-          <p>
-            <a
-              class="btn btn-secondary p-2 mb-2 bg-success text-white"
-              href="/cuidados-covid"
-              role="button"
-              >Saiba mais &raquo;</a
-            >
-          </p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-10 mb-4 text-center">
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal" id="title">Card text</h4>
+          </div>
+          <div class="card-body">
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>
+                <p id="text-card">
+                  Donec id elit non mi porta gravida at eget metus. Fusce
+                  dapibus, tellus ac cursus commodo, tortor mauris condimentum
+                  nibh, ut fermentum massa justo sit amet risus. Etiam porta sem
+                  malesuada magna mollis euismod. Donec sed odio dui
+                </p>
+                <p>
+                  <a
+                    class="btn btn-secondary p-2 mb-2 bg-success text-white"
+                    href="/cuidados-covid"
+                    role="button"
+                    >Saiba mais &raquo;</a
+                  >
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-10 mb-4 text-center">
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal" id="title">Card text</h4>
+          </div>
+          <div class="card-body">
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>
+                <p id="text-card">
+                  Donec id elit non mi porta gravida at eget metus. Fusce
+                  dapibus, tellus ac cursus commodo, tortor mauris condimentum
+                  nibh, ut fermentum massa justo sit amet risus. Etiam porta sem
+                  malesuada magna mollis euismod. Donec sed odio dui
+                </p>
+                <p>
+                  <a
+                    class="btn btn-secondary p-2 mb-2 bg-success text-white"
+                    href="/cuidados-covid"
+                    role="button"
+                    >Saiba mais &raquo;</a
+                  >
+                </p>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-
-    <div class="t1">
-      <h2> DESCRIÇÃO DO PROJETO </h2>
+    <div class="row d-flex justify-content-center">
+      <div class="col-lg-12 col-sm-12 mb-4 text-center">
+        <h1 class="t1">Descrição do Projeto</h1>
+        <p class="texto">
+          O Brasil é a sétima nação mais desigual do mundo atualmente e com
+          <br />
+          a questão da COVID-19 e o isolamento social, a desigualdade cresce
+          <br />
+          cada vez mais. A proposta deste projeto é a disponibilização do Portal
+          <br />
+          da Solidariedade, um ambiente web com a função de dar visibilidade
+          <br />
+          a estas pessoas.
+          <br />
+          <br />
+          O projeto pretende oferecer espaço para que esta população possa ser
+          <br />
+          enxergada, possa disponibilizar suas qualificações, seus saberes,
+          <br />
+          apresentar seus produtos e serviços e, desta forma, conseguir ampliar
+          <br />
+          as suas possibilidades de serem incluídos nos processos de proteção
+          <br />
+          e também de recuperação dos efeitos da pandemia que, seguramente,
+          <br />
+          ainda vão se refletir por vários meses. Também as ações de
+          solidariedade <br />
+          que aos poucos vão acontecendo nas cidades poderão ser hospedadas
+          <br />
+          neste portal. Este conjunto de esforços, todos concentrados em um
+          local, <br />
+          permitindo a ampliação do suporte para os excluídos deste nosso país.
+        </p>
+      </div>
     </div>
-    <div class="alt">
-      <a> O Brasil é a sétima nação mais desigual do mundo atualmente e com <br>
-        a questão da COVID-19 e o isolamento social, a desigualdade cresce <br>
-        cada vez mais. A proposta deste projeto é a disponibilização do Portal <br>
-        da Solidariedade, um ambiente web com a função de dar visibilidade <br>
-        a estas pessoas.
-        <br>
-        <br>
-
-        O projeto pretende oferecer espaço para que esta população possa ser <br>
-        enxergada, possa disponibilizar suas qualificações, seus saberes, <br>
-        apresentar seus produtos e serviços e, desta forma, conseguir ampliar <br>
-        as suas possibilidades de serem incluídos nos processos de proteção <br>
-        e também de recuperação dos efeitos da pandemia que, seguramente, <br>
-        ainda vão se refletir por vários meses. Também as ações de solidariedade <br>
-        que aos poucos vão acontecendo nas cidades poderão ser hospedadas <br>
-        neste portal. Este conjunto de esforços, todos concentrados em um local, <br>
-        permitindo a ampliação do suporte para os excluídos deste
-        nosso país.
-      </a>
-    </div>
-
-    <v-app id="">
-      <Footer ref="drawer"></Footer>
-      <v-content>
-        <router-view></router-view>
-      </v-content>
-    </v-app>
   </div>
 </template>
 
@@ -113,24 +130,31 @@ export default {
 </script>
 
 <style>
-.navbar-default {
+/* .navbar-default {
   margin: 0 !important;
-}
+} */
 
 .container {
   margin-top: 70px;
 }
 
-.alt{
-  margin-top: 50px;
+.texto {
+  margin-top: 15px;
   text-align: center;
   font-family: Arial, Helvetica, sans-serif;
   font-size: 19px;
 }
 
-.t1{
+.t1 {
   margin-top: 25px;
   text-align: center;
 }
 
+/* .btn-secondary {
+  text-align: center;
+} */
+
+.card {
+  margin-right: 5px;
+}
 </style>
